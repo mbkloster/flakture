c22428d95c3533a119b360e8769dbbf795a1bab6
Matthew <matthew.kloster@gmail.com>
2026-08-06 06:51:48 -0700

    Color diamond. Add percentage to distance bar

--- src/render/flakture/control-bar.ts
+++ src/render/flakture/control-bar.ts
@@ -160,6 +160,8 @@ export const renderDistanceFill = (flakture: Flakture, distFillProps: DistanceBa
 export const renderDistanceLine = (flakture: Flakture, distFillProps: DistanceBarProps, side: Side) => {
     const lineElem = flakture.elem(`control-bar-distance-box-line-${ side }`);
     const diamondElem = flakture.elem(`control-bar-distance-box-diamond-${ side }`);
+    const textElem = flakture.elem(`control-bar-distance-box-text-${ side }`);
+    textElem.innerHTML = `${Math.round(distFillProps[side].fillAfterDistance)}%`;
     diamondElem.setAttribute("style", `left: ${ distFillProps[side].fillAfterDistanceAndRefresh }%;`)
     if (distFillProps[side].currentFill === distFillProps[side].fillAfterDistance) {
         lineElem.classList.add('is-invisible');
@@ -226,7 +228,11 @@ export const renderInitialControlBar = (flakture: Flakture) => {
                                 attributes: {"class": `control-bar-distance-box-line control-bar-distance-box-line-${ side } ${ distFillProps[side].currentFill !== distFillProps[side].fillAfterDistance ? "" : "is-invisible"}`, style: `left: ${ distFillProps[side].fillAfterDistance }%;`}
                             }),
                             flakture.createElem("i", {
-                                attributes: {"class": `control-bar-distance-box-diamond control-bar-distance-box-diamond-${ side } fa-solid fa-diamond`, style: `left: ${ distFillProps[side].fillAfterDistanceAndRefresh }%;`}
+                                attributes: {"class": `control-bar-distance-box-diamond control-bar-distance-box-diamond-${ side } is-${ flakture.gameProperties.colors[side] } fa-solid fa-diamond`, style: `left: ${ distFillProps[side].fillAfterDistanceAndRefresh }%;`}
+                            }),
+                            flakture.createElem("span", {
+                                attributes: {"class": `control-bar-distance-box-text control-bar-distance-box-text-${ side }`},
+                                children: [`${Math.round(distFillProps[side].fillAfterDistance)}%`]
                             }),
                         ]
                     })
